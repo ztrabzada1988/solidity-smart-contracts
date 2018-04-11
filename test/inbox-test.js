d8529ec7cc0d6948547418d9cb5244f1023ccaed
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-04-10 22:57:42 -0700

    adding test with intial message on deployed contract

--- test/inbox-test.js
+++ test/inbox-test.js
@@ -1,21 +1,34 @@
 const assert = require('assert'); 
 const ganache = require('ganache-cli'); // local ethereum test network
 const Web3 = require('web3'); // Web3 is a constructor function therefore its capitalized
-const web3 = new Web3(ganache.provider()); //create instance of Web3 and connect to ganache
+const provider = ganache.provider();
+const web3 = new Web3(provider); //create instance of Web3 and connect to ganache
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
-
+    accounts = await web3.eth.getAccounts();     
 
     // Use one of those account to deploy the contract 
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);    
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {});
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call(); // we are calling message method from our Inbox.sol
+        assert.equal(message, 'Hi there');
+    });
 });
 
 
